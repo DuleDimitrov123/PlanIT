bd7d42bfb4d05af069ea6489e96dd089f93cc6ee
DuleDimitrov123 <65020818+DuleDimitrov123@users.noreply.github.com>
2021-08-28 21:09:51 +0200

    Remove unused property

--- backend/PlanIT/PlanIT.Repository/Constants/CompanyColumns.cs
+++ backend/PlanIT/PlanIT.Repository/Constants/CompanyColumns.cs
@@ -12,8 +12,6 @@
 
         public static string Description = "Description";
 
-        public static string TypeOfWorkByCompany = "TypeOfWorkByCompany";
-
         public static string NumberOfWorkplaces = "NumberOfWorkplaces";
     }
 }
